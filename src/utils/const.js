b71cbee26fde49903b4c5b3e9a410d95448b099d
ahianzhang <zhangxinshui@shopin.cn>
2021-02-21 00:44:49 +0800

    update config

--- src/utils/const.js
+++ src/utils/const.js
@@ -47,8 +47,8 @@ export {
 export const AVATAR =
   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU'; // Temp avatar
 export const NAVS = [
-  { text: 'Blog', link: 'https://github.com/yihong0618/running_page' },
-  { text: 'About', link: 'https://github.com/yihong0618/running_page' },
+  { text: 'Blog', link: 'https://www.ahianzhang.com' },
+  { text: 'Twitter', link: 'https://twitter.com/AhianZhang' },
 ];
 
 const nike = 'rgb(224,237,94)';
