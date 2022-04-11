a1221c63f19741b8400495d96eadb84528f8cfe5
ahianzhang <zhangxinshui@shopin.cn>
2022-04-11 18:24:38 +0800

    new

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,18 @@
 module.exports = {
   pathPrefix: '/', // Change to `/running_page` when running on github pages
   siteMetadata: {
-    siteTitle: 'Running Page',
-    siteUrl: 'https://yihong.run',
+    siteTitle: '跑步',
+    siteUrl: 'https://running.ahianzhang.com',
     logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-    description: 'Personal site and blog',
+    description: '个人跑步记录',
     navLinks: [
       {
         name: 'Blog',
-        url: 'https://yihong.run/running',
+        url: 'https://www.ahianzhang.com',
       },
       {
-        name: 'About',
-        url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+        name: 'GitHub',
+        url: 'https://github.com/AhianZhang',
       },
     ],
   },
